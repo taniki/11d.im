2ce0dd01b62b2393e158c14b42292208865b5e0e
taniki <tk@deveha.com>
2020-01-09 10:20:37 +0100

    un peu ménage

--- .eleventy.js
+++ .eleventy.js
@@ -4,21 +4,14 @@ module.exports = function(eleventyConfig) {
     "md",
     "css",
     "woff"
-  ]);
-
-  eleventyConfig.setFrontMatterParsingOptions({
-    sections: true
-  })
+  ])
 
   eleventyConfig.addCollection("notices", collection => collection.getFilteredByGlob("notices/*.md"))
 
   eleventyConfig.addCollection("journal",
-    collection => collection
-      .getAllSorted()
-      .filter(item => item.url
-                    && item.inputPath.startsWith('./journal/')))
+    collection => collection.getFilteredByGlob('./journal/*.md'))
 
   eleventyConfig.addCollection("highlights",
     collection => collection.getFilteredByGlob("highlights/*.md"))
 
-};
+}
