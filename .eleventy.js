5dd3758bda20c38462f0a6594d484fe8d4943111
tk <tk@renard.ninja>
2021-05-14 16:07:38 +0200

    nettoyage du fichier de configuration

--- .eleventy.js
+++ .eleventy.js
@@ -13,23 +13,6 @@ module.exports = function(eleventyConfig) {
     .use(require('./md-tufte/sidenote'))
     .use(require('./md-tufte/marginnote'))
 
-  // md.renderer.rules.footnote_ref = (tokens, idx, options, env, slf) => {
-  //   var id      = slf.rules.footnote_anchor_name(tokens, idx, options, env, slf);
-  //   var caption = slf.rules.footnote_caption(tokens, idx, options, env, slf);
-  //   var refid   = id;
-  //
-  //   if (tokens[idx].meta.subId > 0) {
-  //     refid += ':' + tokens[idx].meta.subId;
-  //   }
-  //
-  //   var label = `<label for="sn-${id}" class="margin-toggle sidenote-number"></label>`
-  //   var input = `<input type="checkbox" id="sn-${id}" class="margin-toggle"/>`
-  //   var sidenote = `<span class="sidenote">${tokens}</span>`
-  //    //return '<sup class="footnote-ref"><a href="#fn' + id + '" id="fnref' + refid + '">' + caption + '</a></sup>';
-  //   return `${label}${input}${sidenote}`
-  // }
-
-
   eleventyConfig.setLibrary("md", md)
 
   eleventyConfig.addCollection("notices", collection => collection.getFilteredByGlob("notices/*.md"))
