a990b73415f5eb58f593838d4aaf7efdd5e4ba83
tk <tk@renard.ninja>
2022-01-06 16:02:10 +0000

    change le format des fichiers `yo`

--- functions/yo.js
+++ functions/yo.js
@@ -1,55 +1,59 @@
-let folder = 'yo'
+let folder = "yo";
 
 function string_to_slug(str) {
-    str = str.replace(/^\s+|\s+$/g, ''); // trim
-    str = str.toLowerCase();
+  str = str.replace(/^\s+|\s+$/g, ""); // trim
+  str = str.toLowerCase();
 
-    // remove accents, swap ñ for n, etc
-    var from = "àáäâèéëêìíïîòóöôùúüûñç·/_,:;";
-    var to   = "aaaaeeeeiiiioooouuuunc------";
-    for (var i=0, l=from.length ; i<l ; i++) {
-        str = str.replace(new RegExp(from.charAt(i), 'g'), to.charAt(i));
-    }
+  // remove accents, swap ñ for n, etc
+  var from = "àáäâèéëêìíïîòóöôùúüûñç·/_,:;";
+  var to = "aaaaeeeeiiiioooouuuunc------";
+  for (var i = 0, l = from.length; i < l; i++) {
+    str = str.replace(new RegExp(from.charAt(i), "g"), to.charAt(i));
+  }
 
-    str = str.replace(/[^a-z0-9 -]/g, '') // remove invalid chars
-        .replace(/\s+/g, '-') // collapse whitespace and replace by -
-        .replace(/-+/g, '-'); // collapse dashes
+  str = str
+    .replace(/[^a-z0-9 -]/g, "") // remove invalid chars
+    .replace(/\s+/g, "-") // collapse whitespace and replace by -
+    .replace(/-+/g, "-"); // collapse dashes
 
-    return str;
+  return str;
 }
 
 let generatePost = (data) => {
-  let date = new Date()
+  const date = new Date();
 
-  let day = (date.getDate()).toString().padStart(2, '0')
-  let month = (date.getMonth()+1).toString().padStart(2, '0')
-  let year = date.getFullYear()
+  const day = date.getUTCDate().toString().padStart(2, "0");
+  const month = (date.getUTCMonth() + 1).toString().padStart(2, "0");
+  const year = date.getUTCFullYear();
+  const hour = date.getUTCHours().toString().padStart(2, "0");
+  const minute = date.getUTCMinutes().toString().padStart(2, "0");
+  const second = date.getUTCSeconds().toString().padStart(2, "0");
 
   let text = `---
 title: ${data.title}
 date: ${year}-${month}-${day}
 ---
 
-${data.content}`
+${data.content}`;
 
-  let slug = `${year}-${month}-${day}-${string_to_slug(data.title)}`
-  let path = `${folder}/${slug}.md`
+  let slug = `${year}${month}${day}${hour}${minute}${second}`;
+  let path = `${folder}/${slug}.md`;
 
-  return { path, slug, text }
-}
+  return { path, slug, text };
+};
 
 exports.handler = async function (event, context) {
   const { Octokit } = require("@octokit/rest");
   const { createTokenAuth } = require("@octokit/auth-token");
   const { request } = require("@octokit/request");
 
-  let data = JSON.parse(event.body)
+  let data = JSON.parse(event.body);
 
-  let { path, slug, text } = generatePost(data)
+  let { path, slug, text } = generatePost(data);
 
-  const TOKEN = data.gh_token
-  const auth = createTokenAuth(TOKEN)
-  const authentication = await auth()
+  const TOKEN = data.gh_token;
+  const auth = createTokenAuth(TOKEN);
+  const authentication = await auth();
   // console.log(authentication)
 
   const octokit = new Octokit({
@@ -57,15 +61,15 @@ exports.handler = async function (event, context) {
   });
 
   const response = await octokit.repos.createOrUpdateFileContents({
-      owner: 'taniki',
-      repo: '11d.im',
-      path: path,
-      message: `✉️👋 yo! ${data.title}`,
-      content: Buffer.from(text, 'utf8').toString('base64')
-  })
+    owner: "taniki",
+    repo: "11d.im",
+    path: path,
+    message: `✉️👋 yo! ${data.title}`,
+    content: Buffer.from(text, "utf8").toString("base64"),
+  });
 
   return {
     statusCode: 200,
-    body: "hey"
+    body: "hey",
   };
 };
