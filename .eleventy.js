a67311ae5f3cc886d3eef54004c62e34980c6c59
duong tam kien <tk@renard.ninja>
2023-01-21 13:46:04 +0100

    remet en forme

--- .eleventy.js
+++ .eleventy.js
@@ -2,55 +2,59 @@ const pluginRss = require("@11ty/eleventy-plugin-rss");
 
 module.exports = function(eleventyConfig) {
 
-  // plugins
-  eleventyConfig.addPlugin(pluginRss);
-  eleventyConfig.addLiquidFilter("dateToRfc3339", pluginRss.dateRfc3339);
+    // plugins
+    eleventyConfig.addPlugin(pluginRss);
+    eleventyConfig.addLiquidFilter("dateToRfc3339", pluginRss.dateRfc3339);
 
-  // supported formats
-  eleventyConfig.setTemplateFormats([
-    "pug",
-    "njk",
-    "md",
-    "css",
-    "woff"
-  ])
+    // supported formats
+    eleventyConfig.setTemplateFormats([
+        "pug",
+        "njk",
+        "md",
+        "css",
+        "woff"
+    ])
 
-  eleventyConfig.addPassthroughCopy("admin")
-  eleventyConfig.addPassthroughCopy("images")
+    eleventyConfig.addPassthroughCopy("admin")
+    eleventyConfig.addPassthroughCopy("images")
 
-  // custom markdown
-  let markdownIt = require("markdown-it")
-  let md = markdownIt({html: true, linkify: true })
-    .use(require('./md-tufte/sidenote'))
-    .use(require('./md-tufte/marginnote'))
+    // custom markdown
+    let markdownIt = require("markdown-it")
+    let md = markdownIt({html: true, linkify: true })
+        .use(require('./md-tufte/sidenote'))
+        .use(require('./md-tufte/marginnote'))
 
-  eleventyConfig.setLibrary("md", md)
+    eleventyConfig.setLibrary("md", md)
 
-  eleventyConfig.addCollection("notices",
-    collection => collection
-      .getFilteredByGlob("notices/*.md")
-      .sort((a,b) => {
-        let x = ((a.data.short) ? a.data.short: a.data.title).toLowerCase()
-        let y = ((b.data.short) ? b.data.short: b.data.title).toLowerCase()
+    eleventyConfig.addCollection("notices",
+        collection => collection
+        .getFilteredByGlob("notices/*.md")
+        .sort((a,b) => {
+            let x = ((a.data.short) ? a.data.short: a.data.title).toLowerCase()
+            let y = ((b.data.short) ? b.data.short: b.data.title).toLowerCase()
 
-        return x.localeCompare(y)
-      })
+            return x.localeCompare(y)
+        })
     )
 
-  eleventyConfig.addCollection("journal",
-    collection => collection
-      .getFilteredByGlob(['./journal/*.md', './yo/*.md'])
-      .filter(x => !x.data.hidden)
+    eleventyConfig.addCollection("journal",
+        collection => collection
+        .getFilteredByGlob(['./journal/*.md', './yo/*.md'])
+        .filter(x => !x.data.hidden)
     )
 
-  eleventyConfig.addCollection("highlights",
-    collection => collection
-      .getFilteredByGlob("highlights/*.md")
-      .sort((a,b)=> {
-        if (a.data.author.last > b.data.author.last) return 1;
-        else if (a.data.author.last < b.data.author.last) return -1;
-        else return 0;
-      })
-  )
+    eleventyConfig.addCollection("highlights",
+        collection => collection
+        .getFilteredByGlob("highlights/*.md")
+        .sort((a,b)=> {
+            if (a.data.author.last > b.data.author.last) return 1;
+            else if (a.data.author.last < b.data.author.last) return -1;
+            else return 0;
+        })
+    )
 
+    eleventyConfig.addCollection("semaines",
+        collection => collection
+        .getFilteredByGlob(['./semaines/*.md'])
+    )
 }
