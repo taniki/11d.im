fd7b23ac9837084fc6a52b4e0c010f591870ca23
tam kien duong <tk@renard.ninja>
2024-10-15 23:13:07 +0200

    importe les contenus de write.apreslanu.it (#55)
    * ajoute le script d'import de la collection `offload`
    * ajoute l'import de `offload`
    * ajoute le script d'import de `tk`
    * ajoute l'import de `tk`
    * ajoute le script d'import de `weeknotes`
    * ajoute l'import de `weeknotes`
    * n'affiche que les contenus de l'année 2024
    * ajoute les imports aux collections existantes
    * ignore les versions notebooks
    * ajoute un lien vers le contenu source

---
+++ scripts/import_wf_tk.py
@@ -0,0 +1,76 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:percent
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.16.4
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+# %%
+import requests
+import frontmatter
+
+from dateutil import parser
+
+# %%
+source = 'https://write.apreslanu.it/api/collections/tk/posts'
+destination = '../content/write.apreslanu.it/tk'
+
+# %%
+total = requests.get(source).json()['data']['total_posts']
+
+total
+
+
+# %%
+def get_posts():
+
+    p = []
+    page = 1
+
+    while (len(p) < total):
+        p += requests.get(source, { 'page': page }).json()['data']['posts']
+        page += 1
+    
+    return p
+
+posts = get_posts()
+
+len(posts)
+
+# %%
+posts[0]
+
+
+# %%
+def write_posts(posts):
+
+    for p in posts:
+        filename = parser.parse(p['created']).strftime('%s')
+        url = source.replace('/api/collections', '').replace('posts', p['slug'])
+        
+        post = frontmatter.Post(
+            p['body'],
+            date = p['created'],
+            title = p['title'],
+            source = url,
+        )
+
+        print(filename)
+        print(post['title'])
+        print(post['source'])
+
+        frontmatter.dump(post, f'{destination}/{filename}.md')
+    
+    return True
+
+write_posts(posts)
+
+# %%
